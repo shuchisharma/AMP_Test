c287d1eaf0f9de5bcbde4d2e54450d41f3a53f35
Shuchi <shuchisharma41@yahoo.com>
2016-01-30 12:00:03 -0700

    Stuff

--- app/src/main/java/com/example/cosorio/myampapplication/MainActivity.java
+++ app/src/main/java/com/example/cosorio/myampapplication/MainActivity.java
@@ -8,8 +8,19 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.util.Log;
+import android.widget.RatingBar;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    static final String console_text = "AMPconsole_string";
+
+    private SeekBar textSize = null;
+    private SeekBar second = null;
+    private TextView textToGrow = null;
+    private RatingBar rate = null;
+    private int FiveStars = 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        Log.i(console_text, "Enter onCreate Function");
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        Log.i(console_text, "Enter onCreateOptionsMenu Function");
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
